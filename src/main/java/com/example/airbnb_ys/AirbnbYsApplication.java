2d31a2cea29f0f25e052ab74b19e76f44b7043a3
youngseo22 <ysnoh22@sookmyung.ac.kr>
2025-04-07 18:08:42 +0900

    commit

---
+++ src/main/java/com/example/airbnb_ys/AirbnbYsApplication.java
@@ -0,0 +1,13 @@
+package com.example.airbnb_ys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirbnbYsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirbnbYsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/airbnb_ys/AirbnbYsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.airbnb_ys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirbnbYsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
