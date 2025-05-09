12b13c2c8ba6544812a1221f52c9252e75527879
youngseo22 <ysnoh22@sookmyung.ac.kr>
2025-05-09 18:51:33 +0900

    feat(main, rooms) : 메인페이지, 상세페이지 개발

---
+++ src/main/java/com/example/airbnb_ys/dto/RatingDto.java
@@ -0,0 +1,43 @@
+package com.example.airbnb_ys.dto;
+
+import com.example.airbnb_ys.model.Review;
+
+import java.util.List;
+
+public class RatingDto {
+    // Todo : 이거 따로 저장해 아님 그때그때 계산해? (난 총점 따로 입력하는 거라고 생각함)
+    private float averageRating;
+    private float ratingClean;
+    private float ratingAccuracy;
+    private float ratingCheckin;
+    private float ratingCommunication;
+    private float ratingLocation;
+    private float ratingPrice;
+
+    public RatingDto(List<Review> reviews){
+        // Todo : 현재 페이지 방에 해당하는 리뷰만 봐야함 (이미 그렇게 되어있는 건가?)
+
+        // Todo : 리뷰 없는 경우 처리해야 함
+        int size = reviews.size();
+        for (Review review : reviews) {
+            this.averageRating += review.getAverageRating();
+            this.ratingClean += review.getRatingClean();
+            this.ratingAccuracy += review.getRatingAccuracy();
+            this.ratingCheckin += review.getRatingCheckIn();
+            this.ratingCommunication += review.getRatingCommunication();
+            this.ratingLocation += review.getRatingLocation();
+            this.ratingPrice += review.getRatingPrice();
+        }
+
+        this.averageRating = round1(averageRating / size);
+        this.ratingClean = round1(ratingClean / size);
+        this.ratingAccuracy = round1(ratingAccuracy / size);
+        this.ratingCheckin = round1(ratingCheckin / size);
+        this.ratingCommunication = round1(ratingCommunication / size);
+        this.ratingLocation = round1(ratingLocation / size);
+        this.ratingPrice = round1(ratingPrice / size);
+    }
+    private float round1(float value) {
+        return Math.round(value * 10f) / 10f; // 소수점 첫째 자리까지 반올림
+    }
+}
