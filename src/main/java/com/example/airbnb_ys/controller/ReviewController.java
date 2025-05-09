d6f75ea3dcae9af2ff9e08a18d21f8ce4a933717
youngseo22 <ysnoh22@sookmyung.ac.kr>
2025-05-10 01:00:25 +0900

    feat(review) : review 기능 구현

---
+++ src/main/java/com/example/airbnb_ys/controller/ReviewController.java
@@ -0,0 +1,11 @@
+package com.example.airbnb_ys.controller;
+
+import com.example.airbnb_ys.service.ReviewService;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ReviewController {
+    private final ReviewService reviewService;
+
+
+}
---
+++ src/main/java/com/example/airbnb_ys/service/ReviewService.java
@@ -0,0 +1,10 @@
+package com.example.airbnb_ys.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ReviewService {
+    public void writeReview(ReviewRequestDto reviewRequestDto){
+
+    }
+}
