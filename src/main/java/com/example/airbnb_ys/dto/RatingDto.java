bedcf0228ab10638955a0823fa2102bd6bd3145b
youngseo22 <ysnoh22@sookmyung.ac.kr>
2025-05-10 23:41:23 +0900

    fix(main, room) : dto 처리들 service로 옮기기

--- src/main/java/com/example/airbnb_ys/dto/RatingDto.java
+++ src/main/java/com/example/airbnb_ys/dto/RatingDto.java
@@ -1,11 +1,10 @@
 package com.example.airbnb_ys.dto;
 
-import com.example.airbnb_ys.model.Review;
+import lombok.AllArgsConstructor;
 
-import java.util.List;
-
+@AllArgsConstructor
 public class RatingDto {
-    private float averageRating;
+    private float rating;
     private float ratingClean;
     private float ratingAccuracy;
     private float ratingCheckin;
@@ -13,30 +12,4 @@ public class RatingDto {
     private float ratingLocation;
     private float ratingPrice;
 
-    public RatingDto(List<Review> reviews){
-        // Todo : 현재 페이지 방에 해당하는 리뷰만 봐야함 (이미 그렇게 되어있는 건가?)
-
-        // Todo : 리뷰 없는 경우 처리해야 함
-        int size = reviews.size();
-        for (Review review : reviews) {
-            this.averageRating += review.getRating();
-            this.ratingClean += review.getRatingClean();
-            this.ratingAccuracy += review.getRatingAccuracy();
-            this.ratingCheckin += review.getRatingCheckIn();
-            this.ratingCommunication += review.getRatingCommunication();
-            this.ratingLocation += review.getRatingLocation();
-            this.ratingPrice += review.getRatingPrice();
-        }
-
-        this.averageRating = round1(averageRating / size);
-        this.ratingClean = round1(ratingClean / size);
-        this.ratingAccuracy = round1(ratingAccuracy / size);
-        this.ratingCheckin = round1(ratingCheckin / size);
-        this.ratingCommunication = round1(ratingCommunication / size);
-        this.ratingLocation = round1(ratingLocation / size);
-        this.ratingPrice = round1(ratingPrice / size);
-    }
-    private float round1(float value) {
-        return Math.round(value * 10f) / 10f; // 소수점 첫째 자리까지 반올림
-    }
 }
--- src/main/java/com/example/airbnb_ys/service/RoomService.java
+++ src/main/java/com/example/airbnb_ys/service/RoomService.java
@@ -1,22 +1,81 @@
 package com.example.airbnb_ys.service;
 
+import com.example.airbnb_ys.dto.RatingDto;
+import com.example.airbnb_ys.dto.ReviewResponseDto;
 import com.example.airbnb_ys.dto.RoomInfoResponseDto;
+import com.example.airbnb_ys.model.Review;
 import com.example.airbnb_ys.model.Room.Room;
+import com.example.airbnb_ys.repository.ReviewRepository;
 import com.example.airbnb_ys.repository.RoomRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class RoomService {
     private final RoomRepository roomRepository;
+    private final ReviewRepository reviewRepository;
 
-    public RoomService(RoomRepository roomRepository) {
+    public RoomService(RoomRepository roomRepository, ReviewRepository reviewRepository) {
         this.roomRepository = roomRepository;
+        this.reviewRepository = reviewRepository;
     }
 
-    public RoomInfoResponseDto getRoomInfo(Long roomId){
+    public RoomInfoResponseDto getRoomInfo(int roomId){
         Room room = roomRepository.findById(roomId)
                 .orElseThrow(() -> new IllegalArgumentException("방 존재 X"));
-        // 이렇게 서비스단에서 뭐 안하고 디티오로 처리해도 되는 건가 ...
-        return new RoomInfoResponseDto(room);
+
+        // 별점 (평균) 계산하기
+        List<Review> allReviews = reviewRepository.findByRoomId(roomId).stream().toList();
+        RatingDto ratings = calRatings(allReviews);
+
+        // 리뷰 받아오기
+        // Todo : 현재 페이지 방에 해당하는 리뷰만 봐야함 (이미 그렇게 되어있는 건가?)
+        List<ReviewResponseDto> reviews = room.getReviews().stream()
+                .map(ReviewResponseDto::new)
+                .collect(Collectors.toList());
+
+        return new RoomInfoResponseDto(room, ratings, reviews);
+    }
+
+    public RatingDto calRatings(List<Review> allReviews){
+        int size = allReviews.size();
+
+        float rating = 0;
+        float ratingClean = 0;
+        float ratingAccuracy = 0;
+        float ratingCheckin = 0;
+        float ratingCommunication = 0;
+        float ratingLocation = 0;
+        float ratingPrice = 0;
+
+        // Todo : 리뷰 없는 경우 처리해야 함
+        if (size == 0)
+            return new RatingDto(0, 0, 0, 0, 0, 0, 0);
+
+        for (Review review : allReviews) {
+            rating += review.getRating();
+            ratingClean += review.getRatingClean();
+            ratingAccuracy += review.getRatingAccuracy();
+            ratingCheckin += review.getRatingCheckIn();
+            ratingCommunication += review.getRatingCommunication();
+            ratingLocation += review.getRatingLocation();
+            ratingPrice += review.getRatingPrice();
+        }
+
+        return new RatingDto(
+            round1(rating / size),
+            round1(ratingClean / size),
+            round1(ratingAccuracy / size),
+            round1(ratingCheckin / size),
+            round1(ratingCommunication / size),
+            round1(ratingLocation / size),
+            round1(ratingPrice / size)
+        );
+    }
+
+    private float round1(float value) {
+        return Math.round(value * 10f) / 10f; // 소수점 첫째 자리까지 반올림
     }
 }
